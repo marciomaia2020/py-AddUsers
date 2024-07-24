8574cc3d0a275ef5d7a5a5cefa951d2bd8fb8510
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2024-07-24 16:10:00 -0300

    Alteração nos arquivos de configuração pra ver se roda a aplicação

--- projeto/app.py
+++ projeto/app.py
@@ -43,15 +43,15 @@ def add_user():
     db.session.commit()
     return redirect(url_for('index'))
 
-
+"""
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
-
 """
+
+
+
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0')
-"""
+    #app.run(debug=False,host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
