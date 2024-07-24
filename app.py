9e35f6a959e961b3b4a95662400cea0e27430d4b
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2024-07-24 11:27:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,14 +43,15 @@ def add_user():
     db.session.commit()
     return redirect(url_for('index'))
 
-"""
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+
+
 """
-
-
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
-
+"""
